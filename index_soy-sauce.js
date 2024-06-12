8199e5c4e63dd2ecf499d661e57d65e99f7389d8
Shelby Wilson <s.wilson024@gmail.com>
2024-06-11 22:01:17 -0700

    add soy sauce

---
+++ index_soy-sauce.js
@@ -0,0 +1,196 @@
+// 🪩 fork from Ksenia Kondrashova https://codepen.io/ksenia-k/pen/ZEjJxWQ
+
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls'
+import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+
+const container = document.querySelector('.container');
+const canvasEl = document.querySelector('#canvas');
+
+let renderer, clock, texture, scenes = [];
+
+const loader = new OBJLoader();
+
+let objects = [null];
+
+loader.load('models/Soy sauce.obj', function (obj) {
+    objects[0] = 
+    {
+        geometry: {...obj},
+        mirrorSize: .0011,
+        isObject: true,
+    };
+
+    init();
+});
+
+
+function init() {
+    initScene();
+    window.addEventListener('resize', updateSceneSize);
+
+    function initScene() {
+
+        renderer = new THREE.WebGLRenderer({
+            alpha: true,
+            antialias: true,
+            canvas: canvasEl
+        });
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+        clock = new THREE.Clock();
+
+        new THREE.TextureLoader().load(
+            'bg3.png',
+            (t) => {
+                texture = t;
+                for (let i = 0; i < objects.length; i++) {
+                    scenes.push(createScene(i));
+                }
+                updateSceneSize();
+                render();
+            });
+    }
+
+    function createDiscoBall(idx) {
+        const dummy = new THREE.Object3D();
+
+        const mirrorMaterial = new THREE.MeshMatcapMaterial({
+            matcap: texture
+        });
+        
+        const object = objects[idx];
+        let geometryOriginal =  object.isObject ? object.geometry.children[0].geometry : object.geometry;
+        geometryOriginal.deleteAttribute('normal');
+        geometryOriginal.deleteAttribute('uv');
+        geometryOriginal = BufferGeometryUtils.mergeVertices(geometryOriginal);
+        geometryOriginal.computeVertexNormals();
+        // geometryOriginal.translateY(-0.1);
+
+        const mirrorGeometry = new THREE.PlaneGeometry(objects[idx].mirrorSize, objects[idx].mirrorSize);
+        let instancedMirrorMesh = new THREE.InstancedMesh(
+            mirrorGeometry,
+            mirrorMaterial,
+            geometryOriginal.attributes.position.count
+        );
+
+        const positions = geometryOriginal.attributes.position.array;
+        const normals = geometryOriginal.attributes.normal.array;
+        for (let i = 0; i < positions.length; i += 3) {
+            dummy.position.set(positions[i], positions[i + 1], positions[i + 2]);
+            dummy.lookAt(positions[i] + normals[i], positions[i + 1] + normals[i + 1], positions[i + 2] + normals[i + 2]);
+            dummy.updateMatrix();
+            instancedMirrorMesh.setMatrixAt(i / 3, dummy.matrix);
+        }
+
+        const obj = new THREE.Group();
+        const innerGeometry = geometryOriginal.clone();
+        const ballInnerMaterial = new THREE.MeshBasicMaterial({ color: 0x222222 });
+        const innerMesh = new THREE.Mesh(
+            innerGeometry,
+            ballInnerMaterial
+        );
+        obj.add(innerMesh, instancedMirrorMesh);
+
+        return obj;
+    }
+
+    function createScene(geometryIdx) {
+        const scene = new THREE.Scene();
+
+        const element = document.createElement('div');
+        element.className = 'overlay-block';
+        scene.userData.element = element;
+        container.appendChild(element);
+
+        const camera = new THREE.PerspectiveCamera(22, scene.userData.element.clientWidth / scene.userData.element.clientHeight, 1, 10);
+        // camera.position.y = 3
+        // camera.position.z = 2;
+        scene.userData.camera = camera;
+
+        const controls = new OrbitControls(scene.userData.camera, scene.userData.element);
+        controls.minDistance = 2;
+        controls.maxDistance = 5;
+        // controls.autoRotate = true;
+        controls.autoRotateSpeed = 6;
+        controls.enableZoom = false;
+        controls.enableDamping = true;
+        scene.userData.controls = controls;
+
+        scene.rotateOnAxis(new THREE.Vector3(1, 0, 0), -Math.PI / 2);
+        scene.translateY(-0.2)
+
+        scene.add(createDiscoBall(geometryIdx));
+
+        return scene;
+    }
+
+    function render() {
+
+        renderer.setScissorTest(true);
+        const delta = .04 * clock.getDelta();
+        
+        scenes.forEach((scene, idx) => {
+            const rect = scene.userData.element.getBoundingClientRect();
+            const width = rect.right - rect.left;
+            const height = rect.bottom - rect.top;
+            const left = rect.left;
+            const bottom = renderer.domElement.clientHeight - rect.bottom;
+
+            renderer.setViewport(left, bottom, width, height * 2);
+            renderer.setScissor(left, bottom, width, height * 2);
+
+            scene.userData.controls.update();
+
+            // if (idx === 0) {
+            //     scene.children[0].rotateX(delta);
+            //     scene.children[0].rotateZ(delta);
+            // } else if (idx === 2) {
+            //     scene.children[0].rotateZ(-3 * delta);
+            // }
+            // scene.children[0].position.x = Math.cos(clock.getElapsedTime() / 1 + (Math.sin(idx) * 5)) * 0.05;
+            // scene.children[0].rotation.z += (Math.cos(clock.getElapsedTime() / 10  + idx / 10) + 1) / 2 * Math.PI / 40;
+
+            // scene.children[0].rotation.z += delta * Math.cos(idx)
+
+            const scale = window.innerWidth < 768 ? 0.75 : 1.5;
+            scene.children[0].scale.x = scale;
+            scene.children[0].scale.y = scale;
+            scene.children[0].scale.z = scale;
+
+
+            // scene.children[0].position.z = 0.
+            // scene.children[0].rotation.x = (-Math.PI/2);
+
+            // if (idx === 0) {
+                // scene.children[0].rotateZ(16 * delta);
+                scene.children[0].rotateY(-6 * delta);
+            // } else if (idx === 1) {
+                // scene.children[0].rotateY(-10 * delta);
+                // scene.children[0].rotateX(-6 * delta);
+            // } else if (idx === 2) {
+            //     scene.children[0].rotateX(-7 * delta);
+            // } else if (idx === 3) {
+            //     scene.children[0].rotateY(12 * delta);
+            //     scene.children[0].rotateZ(3 * delta);
+            // }
+
+            // scene.children[0].position.z = Math.sin(delta) * 0.1;
+
+            renderer.render(scene, scene.userData.camera);
+        });
+
+        requestAnimationFrame(render);
+    }
+
+    function updateSceneSize() {
+        scenes.forEach(scene => {
+            scene.userData.camera.aspect = scene.userData.element.clientWidth / (scene.userData.element.clientHeight*2);
+            scene.userData.camera.updateProjectionMatrix();
+        });
+        renderer.setSize(container.clientWidth, container.clientHeight);
+    }
+
+};
