16da39c4becc691306bd73e871f22a79787e08c6
Shelby Wilson <s.wilson024@gmail.com>
2024-06-12 12:38:31 -0700

    updates

--- index_soy-sauce.js
+++ index_soy-sauce.js
@@ -3,7 +3,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls'
 import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
 const container = document.querySelector('.container');
@@ -16,10 +15,10 @@ const loader = new OBJLoader();
 let objects = [null];
 
 loader.load('models/Soy sauce.obj', function (obj) {
-    objects[0] = 
+    objects[0] =
     {
-        geometry: {...obj},
-        mirrorSize: .0011,
+        geometry: { ...obj },
+        mirrorSize: .0012,
         isObject: true,
     };
 
@@ -60,14 +59,13 @@ function init() {
         const mirrorMaterial = new THREE.MeshMatcapMaterial({
             matcap: texture
         });
-        
+
         const object = objects[idx];
-        let geometryOriginal =  object.isObject ? object.geometry.children[0].geometry : object.geometry;
+        let geometryOriginal = object.isObject ? object.geometry.children[0].geometry : object.geometry;
         geometryOriginal.deleteAttribute('normal');
         geometryOriginal.deleteAttribute('uv');
         geometryOriginal = BufferGeometryUtils.mergeVertices(geometryOriginal);
         geometryOriginal.computeVertexNormals();
-        // geometryOriginal.translateY(-0.1);
 
         const mirrorGeometry = new THREE.PlaneGeometry(objects[idx].mirrorSize, objects[idx].mirrorSize);
         let instancedMirrorMesh = new THREE.InstancedMesh(
@@ -87,11 +85,26 @@ function init() {
 
         const obj = new THREE.Group();
         const innerGeometry = geometryOriginal.clone();
-        const ballInnerMaterial = new THREE.MeshBasicMaterial({ color: 0x222222 });
+        const ballInnerMaterial = new THREE.MeshBasicMaterial({ color: 0x452624 });
         const innerMesh = new THREE.Mesh(
             innerGeometry,
             ballInnerMaterial
         );
+        const rightEye = new THREE.Mesh(new THREE.SphereGeometry(0.0042, 32, 32), new THREE.MeshPhongMaterial({ color: 0x666666, roughness: 0, metalness: 1.0 }));
+        rightEye.position.set(-0.01, 0.087, 0.026);
+        const leftEye = rightEye.clone();
+        leftEye.position.x = 0.013;
+        leftEye.position.y = 0.084;
+        leftEye.position.z = 0.028;
+        const nose = new THREE.Mesh(new THREE.SphereGeometry(0.0045, 32, 32), new THREE.MeshPhongMaterial({ color: 0x666666, roughness: 1.0, metalness: 1.0 }));
+        nose.scale.set(1.4, 1.4, 1.4);
+        nose.position.y = 0.078;
+        nose.position.z = 0.04;
+        nose.position.x = 0.001;
+        innerMesh.add(rightEye);
+        innerMesh.add(leftEye);
+        innerMesh.add(nose);
+
         obj.add(innerMesh, instancedMirrorMesh);
 
         return obj;
@@ -99,6 +112,7 @@ function init() {
 
     function createScene(geometryIdx) {
         const scene = new THREE.Scene();
+        scene.position.y = -0.25;
 
         const element = document.createElement('div');
         element.className = 'overlay-block';
@@ -107,22 +121,38 @@ function init() {
 
         const camera = new THREE.PerspectiveCamera(22, scene.userData.element.clientWidth / scene.userData.element.clientHeight, 1, 10);
         // camera.position.y = 3
+        // camera.position.x = 0;
         // camera.position.z = 2;
+        // camera.up.set(0 , 0, 0 );
+        let offset = new THREE.Vector3( 0, 0.1, -1.5 )
+
+        camera.position.addVectors( scene.position, offset )
+
         scene.userData.camera = camera;
 
         const controls = new OrbitControls(scene.userData.camera, scene.userData.element);
-        controls.minDistance = 2;
-        controls.maxDistance = 5;
-        // controls.autoRotate = true;
-        controls.autoRotateSpeed = 6;
-        controls.enableZoom = false;
+        controls.minDistance = 1.25;
+        controls.maxDistance = 10;
+        controls.autoRotate = true;
+        controls.autoRotateSpeed = 3;
+        controls.enableZoom = true;
         controls.enableDamping = true;
+        controls.enableRotate = true;
         scene.userData.controls = controls;
 
-        scene.rotateOnAxis(new THREE.Vector3(1, 0, 0), -Math.PI / 2);
-        scene.translateY(-0.2)
-
         scene.add(createDiscoBall(geometryIdx));
+
+        // var hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
+        // hemiLight.position.set(0, -5, 0);
+    
+        // scene.add(hemiLight);
+    
+        var dirLight = new THREE.DirectionalLight(0xffffff, 0.54);
+        dirLight.position.set(-5, 12, 5);
+        dirLight.castShadow = true;
+        dirLight.shadow.mapSize = new THREE.Vector2(5524, 5524);
+    
+        scene.add(dirLight);
 
         return scene;
     }
@@ -131,7 +161,7 @@ function init() {
 
         renderer.setScissorTest(true);
         const delta = .04 * clock.getDelta();
-        
+
         scenes.forEach((scene, idx) => {
             const rect = scene.userData.element.getBoundingClientRect();
             const width = rect.right - rect.left;
@@ -155,21 +185,22 @@ function init() {
 
             // scene.children[0].rotation.z += delta * Math.cos(idx)
 
-            const scale = window.innerWidth < 768 ? 0.75 : 1.5;
+            const scale = window.innerWidth < 768 ? 1 : 1.75;
             scene.children[0].scale.x = scale;
             scene.children[0].scale.y = scale;
             scene.children[0].scale.z = scale;
+            scene.children[0].rotation.y = Math.PI;
 
 
             // scene.children[0].position.z = 0.
             // scene.children[0].rotation.x = (-Math.PI/2);
 
             // if (idx === 0) {
-                // scene.children[0].rotateZ(16 * delta);
-                scene.children[0].rotateY(-6 * delta);
+            // scene.children[0].rotateZ(16 * delta);
+            // scene.children[0].rotateY(6 * delta);
             // } else if (idx === 1) {
-                // scene.children[0].rotateY(-10 * delta);
-                // scene.children[0].rotateX(-6 * delta);
+            // scene.children[0].rotateY(-10 * delta);
+            // scene.children[0].rotateX(-6 * delta);
             // } else if (idx === 2) {
             //     scene.children[0].rotateX(-7 * delta);
             // } else if (idx === 3) {
@@ -187,8 +218,11 @@ function init() {
 
     function updateSceneSize() {
         scenes.forEach(scene => {
-            scene.userData.camera.aspect = scene.userData.element.clientWidth / (scene.userData.element.clientHeight*2);
+            scene.userData.camera.aspect = scene.userData.element.clientWidth / (scene.userData.element.clientHeight * 2);
             scene.userData.camera.updateProjectionMatrix();
+
+            // scene.position.z = 0.17;
+            // scene.rotation.x = -Math.PI / 2;    
         });
         renderer.setSize(container.clientWidth, container.clientHeight);
     }
